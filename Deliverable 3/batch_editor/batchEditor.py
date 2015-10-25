7a2be096722dfc762dfb478a463e07ff3aa477b9
Daniel <dev.persaud@mail.utoronto.ca>
2015-10-25 02:39:59 -0400

    made batchEditor into a well formatted class

--- Deliverable 3/batch_editor/batchEditor.py
+++ Deliverable 3/batch_editor/batchEditor.py
@@ -2,29 +2,63 @@
 #install it by typing pip install pyzotero in cmd line or terminal
 #or see https://github.com/urschrei/pyzotero for instructions
 
+import os
 import sys
 import argparse
 from pyzotero import zotero
 
-user_id = '2704725';
-user_type = 'user';
-api_key = 'Zt1Q6xrcy28OOc4zRNszKbZL';
+class BEditor:
 
-class BatchConfig:
+	def __init__(self, user_id='', user_type='', api_key=''):
 
+		path = 'data/user_data'
 
-	def config():
-		''' sets the user_id, user_type, and API Key so that program can
-			access the user's zotero library.
+		#data for accessing zotero library
+		self._user_id = user_id;
+		self._user_type = user_type;
+		self._api_key = api_key;
+
+		if os.path.exists(path):
+			#get the user's library data from file
+			user_data = [line.rstrip('\n') for line in open(path)]
+		if len(user_data) == 3:
+			self._user_key = user_data[0];
+			self._user_type = user_data[1];
+			self._api_key = user_data[2]; 
+
+		else:
+			if (len(user_id) and len(user_type) and len(api_key)):
+				#no data exists, create file and directory to store user's data
+				if not os.path.exists(os.path.dirname(path)):
+					os.makedirs(os.path.dirname(path))
+
+				f = open(path, 'w')
+				f.write(user_id + '\n');
+				f.write(user_type + '\n');
+				f.write(api_key + '\n');
+				f.close()
+
+		#initialize a connection to library and validate connection
+		try:
+			self._zot = zotero.Zotero(user_id, user_type, api_key)
+		except Exception, err:
+			raise
+		self.test_config()
+
+	def test_config(self):
+		''' Attempts to access Zotero library with user's ID & TYPE & APIKEY
+			raises error if invalid credentials
 	    '''
-		pass
+		try: 
+			self._zot.groups()
+		except Exception, err:
+			raise
 
-
-	def batch_editor(old_tag, new_tag):
+	def batch_editor(self, old_tag, new_tag):
 		''' this takes all items with tag, old_tag and updates it so
 		    that old_tag is replaced by new_tag
 		'''
-		items = zot.items();
+		items = self._zot.items();
 
 		#for each item in the library access the list containing all of its tag information
 		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
@@ -33,35 +67,20 @@ class BatchConfig:
 				#replace old tag with new tagname with new_tag if applicable
 				if tag['tag'] == old_tag:
 					tag['tag'] = new_tag
-					zot.update_item(item)
+					self._zot.update_item(item)
 
 
-	def batch_edit_collection(collection, old_tag, new_tag):
+	def batch_edit_collection(self, collection, old_tag, new_tag):
 		''' replaces all tags with value old_tag with value new_tag in specified
 			collection and updates the library
 		'''
 		pass
 
-	def backup_library(): #optional
+	def backup_library(self): #optional
 		''' backup entire library before making changes '''
 		#will need write permissions?
 		pass
 
-	def restore_library(): #optional
+	def restore_library(self): #optional
 		''' restore entire library to its state before changes were made'''
 		pass
-
-
-#create a zotero instance with params user-id, user-type, API Key
-zot = zotero.Zotero(user_id, user_type, api_key)
-
-#deprecated or some shit
-# #takes command line args as inputs or you can just change the parameters in
-# #the else statement see @batch_editor method for usage
-# #the format for command line args are as follow: python batchEditor.py old_tag new_tag
-# if (len(sys.argv) == 3):
-# 	batch_editor(sys.argv[1], sys.argv[2])
-# else:
-# 	batch_editor('test', '__test')
-
-#implement using argparse
