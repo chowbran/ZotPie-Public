fd24f6d31dd4d4848645552027d73558f8b8370b
Kenny Lam <kenny.lam@mail.utoronto.ca>
2015-10-26 14:11:15 -0400

    Added user data information

--- ZotPie UI/ZotPieApp/ZotPieApp/mainStartWin.py
+++ ZotPie UI/ZotPieApp/ZotPieApp/mainStartWin.py
@@ -1,9 +1,10 @@
-from PyQt4 import QtCore, QtGui
+﻿from PyQt4 import QtCore, QtGui
 from mainStart import Ui_StartWindow
 from batchEditorWin import BatchEditorWin
 from citationEditorWin import CitationEditorWin
 #from coupleDocumentsWin import CoupleDocumentsWin
 from preferencesWin import PreferencesWin
+from userData import UserData
 
 import sys
 
@@ -11,12 +12,16 @@ class MainStartWin(QtGui.QMainWindow, Ui_StartWindow):
     def __init__(self, parent=None):
         super(MainStartWin, self).__init__(parent)
         self.setupUi(self)
-        self.eventHandlerSetup();
+        self.eventHandlerSetup()
+        self.userData = UserData()
+        self.userData.setKeyValue("GENERAL", 'apikey2', '1111222221122')
+        print(self.userData.getValue("GENERAL", "apikey"))
 
     def eventHandlerSetup(self):
         self.btn_OpenBatchEditor.clicked.connect(lambda: self.openWindow(BatchEditorWin(self)))
         self.btn_OpenCitationEditor.clicked.connect(lambda: self.openWindow(CitationEditorWin(self)))
         #self.btn_OpenCoupleDocuments.clicked.connect(lambda: self.openWindow(CoupleDocumentsWin()))
+        #self.btn_OpenCoupleDocuments.clicked.connect(lambda: self.testuserdata())
         self.btn_Prefs.clicked.connect(lambda: self.openWindow(PreferencesWin(self)))
         self.btn_Quit.clicked.connect(lambda: sys.exit(0))
 
