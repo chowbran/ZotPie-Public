785d8a41182338140b50ebf4e9c8c71cb1d20a9d
Kenny Lam <kenny.lam@mail.utoronto.ca>
2015-10-26 19:45:22 -0400

    Linked UI buttons to functions

--- ZotPie UI/ZotPieApp/ZotPieApp/coupleDocumentsWin.py
+++ ZotPie UI/ZotPieApp/ZotPieApp/coupleDocumentsWin.py
@@ -1,9 +1,6 @@
 ﻿from PyQt4 import QtCore, QtGui
 from coupleDocuments import Ui_CoupleDocumentsWindow
-from pyzotero import zotero
 from couple import CoupleDocuments
-
-zot = zotero.Zotero('2720485', 'user', 'amxUEQbOcgQShX0Xd0zuQDvR')
 
 class CoupleDocumentsWin(QtGui.QMainWindow, Ui_CoupleDocumentsWindow):
     def __init__(self, parent=None):
