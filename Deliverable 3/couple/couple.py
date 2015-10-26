1b8f2c515b59b807a1cbc56a80759501da8df90b
yongzhe.zheng1@gmail.com <yongzhe.zheng1@gmail.com>
2015-10-26 17:04:21 -0400

    changes to coupling documents for groups

--- Deliverable 3/couple/couple.py
+++ Deliverable 3/couple/couple.py
@@ -26,7 +26,8 @@ class CoupleDocuments:
 		self.uid = uid
 		self.key = key
 		self.zot = zotero.Zotero(uid, utype, key)
-
+		if utype == 'user':
+			self.zotgroup = zotero.Zotero(uid, 'group', key)
 
 	def populateGroupCollections(self):
 		'''populates all of groups associated with the user
@@ -35,7 +36,7 @@ class CoupleDocuments:
 		groupAccessName = []
 
 		for group in listofGroups:
-			groupAccessName.append(group['data']['name'] + ";" + group['data']['id'])
+			groupAccessName.append([group['data']['name'], group['data']['id']])
 
 		return groupAccessName
 
@@ -43,28 +44,31 @@ class CoupleDocuments:
 		'''given a group, we return all record names that are associated
 		   with that group
 		'''
-		groupAccess = zotero.Zotero(self.uid, 'group', self.key)
+
 		recordNames = []
 
-		for record in groupAccess.items():
+		for record in self.groupAccess.items():
 			recordNames.append(record['data']['title'])
 
 		return recordNames
 
 
 	def jsonFileUpdater(self, key, documentList):
-		'''takes in a key of an original document and a list of all documents
-		   in groups to be coupled. Creates a json file if it doesn't exist
+		'''takes in a key of an original document and a list of dictionaries groups
+		   and files to be coupled in those groups. Creates a json file if it doesn't exist
 		   otherwise, searches through json to find the key to grab the list.
-		   updates the list with new coupled documents if necessary. Returns
+		   updates the list with new coupled documents. Returns
 		   a list with the key.
 		'''
 
 		if (os.path.isfile('keys.json')):
 			json_file = open('keys.json', 'r')
-			json_str = json.read()
-			json_dic = json.loads(json_str)[0]
+			json_str = json_file.read()
+			json_dic = json.loads(json_str)
 
+
+			#look for the record to be copied, if it exits, then we
+			#update the list of dictionaries for that record
 			for itemkey, value in json_dic.iteritems():
 				if itemkey == key:
 					value = documentList
@@ -80,7 +84,7 @@ class CoupleDocuments:
 
 		return json_dic
 
-	def jsonFileGetter(self, key):
+	def jsonKeyGetter(self, key):
 		'''Given a key, goes through the json file to find the corresponding
 			list of keys
 		'''
@@ -106,18 +110,19 @@ class CoupleDocuments:
 		'''
 
 		rejects = []
-		coupledDocuments = self.jsonFileGetter(key)
+		coupledDocuments = self.jsonKeyGetter(key)
 
 		#find the original document
-		original = zot.item(key)
+		original = self.zot.item(key)
 
 		#go through every document in coupleDocuments
 		for document in coupleDocuments:
 		#check their type is equal to original
-			copy = zotgroup.item(document)
+			groupId = document.keys()[0]
+			copy = self.groupAccess().item(document[document[groupId]])
 			if copy[0]['itemType'] == original[0]['itemType']:
 				itemType = copy[0]['itemType']
-				validFields = zot.item_type_fields(itemType)[0]
+				validFields = self.zot.item_type_fields(itemType)[0]
 				#if so, change all the fields to be the same as the original
 				for field in validFields:
 					copy[0][field] = original[0][field]
@@ -134,3 +139,4 @@ if __name__ == "__main__":
 	test = CoupleDocuments()
 	test.config('2662347',  'DBKmDBQRaQXSMwGB9UMroGJ3')
 	test.populateGroupCollections()
+	test.jsonFileUpdater('key', [{'group1':'key1'}, {'group2':'key2'}])
