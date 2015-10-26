36a37e78670ffb5568c93fb54e4c13335dc1a296
Daniel Persaud <dev.persaud@mail.utoronto.ca>
2015-10-26 19:49:18 -0400

    added featue to batch edit any field

--- ZotPie UI/ZotPieApp/ZotPieApp/batch_editor/BEditor.py
+++ ZotPie UI/ZotPieApp/ZotPieApp/batch_editor/BEditor.py
@@ -108,8 +108,6 @@ class BEditor:
 		'''
 		items = self._zot.items();
 
-
-
 		#for each item in the library access the list containing all of its tag information
 		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
 		for item in items:
@@ -119,9 +117,9 @@ class BEditor:
 	def batch_edit_collection(self, collection, old_tag, new_tag):
 		''' replaces all tags with value old_tag with value new_tag in specified
 			collection and updates the library
-		'''
+		''' #UNTESTED
 
-		collections = self.zot.collections();
+		collections = self._zot.collections();
 		collectionID = collections[0]['data']['key']
 
 		# Find the collectionID of collection collection
@@ -130,11 +128,11 @@ class BEditor:
 				collectionID = coll['data']['key']
 
 		# Edit the tags old_tag in the collection identified by collectionID
-		for item in self.zot.collection_items(collectionID):#[1]['data']['tags']
+		for item in self._zot.collection_items(collectionID):#[1]['data']['tags']
 			for tag in item['data']['tags']:
 				if tag['tag'] == old_tag:
 					tag['tag'] = new_tag
-					self.zot.update_item(item)
+					self._zot.update_item(item)
 
 	def backup_library(self): #untested
 		''' backup entire library, this overwrites any previous backup'''
@@ -156,37 +154,14 @@ class BEditor:
 		for item in data:
 			self._zot.update_item(item)
 
-
-#create a zotero instance with params user-id, user-type, API Key
-
-#deprecated or some shit
-# #takes command line args as inputs or you can just change the parameters in
-# #the else statement see @batch_editor method for usage
-# #the format for command line args are as follow: python batchEditor.py old_tag new_tag
-# if (len(sys.argv) == 3):
-# 	batch_editor(sys.argv[1], sys.argv[2])
-# else:
-# 	batch_editor('test', '__test')
-
-#implement using argparse
-# if __name__ == "__main__":
-# 	parser = argparse.ArgumentParser(description='Process some integers.')
-# 	parser.add_argument("-user", nargs='?', default="2704725", help='Entry for user id')
-# 	parser.add_argument("-apikey", nargs='?', default="Zt1Q6xrcy28OOc4zRNszKbZL", help='Entry for api key')
-# 	parser.add_argument("func", default="batch_editor", help='Entry for api key')
-# 	parser.add_argument("argv", nargs='*', default="Zt1Q6xrcy28OOc4zRNszKbZL", help='Entry for api key')
-
-# 	args = parser.parse_args()
-
-# 	print args.user
-# 	print args.apikey
-# 	print args.func
-# 	print args.argv
-
-# 	func = args.func;
-# 	batch = BatchConfig()
-# 	batch.config(args.user, args.apikey)
-# 	argv = args.argv
-
-# 	if func == "batch_editor":
-# 		batch_editor()
+	def batch_edit(self, field, old_value, new_value):
+		''' batch edit given field for entire library '''
+		items = self._zot.items()
+		#for each item in the library access the list containing all of its tag information
+		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
+		for item in items:
+			for tag in item['data'][field]:
+				#replace old tag with new tagname with new_tag if applicable
+				if tag[field] == old_value:
+					tag[field] = new_value
+					self._zot.update_item(item)
