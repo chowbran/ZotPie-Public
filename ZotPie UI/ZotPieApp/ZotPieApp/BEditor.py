929f85c33d52f669b4a4a4726ad2f03cf43e2b54
chowbran <brandon.chow@mail.utoronto.ca>
2015-10-27 18:49:29 -0400

    Added tag filterings when searching for items.

--- ZotPie UI/ZotPieApp/ZotPieApp/BEditor.py
+++ ZotPie UI/ZotPieApp/ZotPieApp/BEditor.py
@@ -29,6 +29,8 @@ class BEditor:
 		#initialize a connection to library and validate connection
 		try:
 			self._zot = zotero.Zotero(self.userData.getValue('GENERAL', 'libraryid'), 'user', self.userData.getValue('GENERAL', 'apikey'))
+	        # self._zot = zotero.Zotero('2710002', 'user', 'jxIEnHTfXW5guwz6X8q5upsv')
+
 		except Exception, err:
 			raise
 		self.test_config()
@@ -64,22 +66,21 @@ class BEditor:
 		''' this takes all items with tag, old_tag and updates it so
 		    that old_tag is replaced by new_tag
 		'''
-		items = self._zot.items()
+		items = self._zot.items(tag=old_tag)
 
 		#for each item in the library access the list containing all of its tag information
 		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
 		for item in items:
 			for tag in item['data']['tags']:
 				#replace old tag with new tagname with new_tag if applicable
-				if tag['tag'] == old_tag:
-					tag['tag'] = new_tag
-					self._zot.update_item(item)
+				tag['tag'] = new_tag
+				self._zot.update_item(item)
 
 	def delete_tag(self, del_tag):
 		''' (self, str) -> None
 			this takes all items with del_tag and deletes them.
 		'''
-		items = self._zot.items();
+		items = self._zot.items(tag=del_tag);
 
 		#for each item in the library access the list containing all of its tag information
 		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
@@ -99,24 +100,12 @@ class BEditor:
 		''' return entire library's raw data '''
 		return self._zot.items();
 
-	def delete_tag(self, del_tag):
-		''' (BEditor, str) -> None
-			this takes all items with del_tag and deletes them.
-		'''
-		items = self._zot.items();
-
-		#for each item in the library access the list containing all of its tag information
-		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
-		for item in items:
-			tags = item['data']['tags']
-			tags[:] = [d for d in tags if d.get('tags') != del_tag]
-
 	def batch_edit_collection(self, collection, old_tag, new_tag):
 		''' replaces all tags with value old_tag with value new_tag in specified
 			collection and updates the library
 		''' #UNTESTED
 
-		collections = self._zot.collections();
+		collections = self._zot.collections(tag=old_tag);
 		collectionID = collections[0]['data']['key']
 
 		# Find the collectionID of collection collection
@@ -127,9 +116,8 @@ class BEditor:
 		# Edit the tags old_tag in the collection identified by collectionID
 		for item in self._zot.collection_items(collectionID):#[1]['data']['tags']
 			for tag in item['data']['tags']:
-				if tag['tag'] == old_tag:
-					tag['tag'] = new_tag
-					self._zot.update_item(item)
+				tag['tag'] = new_tag
+				self._zot.update_item(item)
 
 	def backup_library(self): #untested
 		''' backup entire library, this overwrites any previous backup'''
@@ -153,6 +141,7 @@ class BEditor:
 
 	def batch_edit(self, field, old_value, new_value):
 		''' batch edit given field for entire library '''
+
 		items = self._zot.items()
 		#for each item in the library access the list containing all of its tag information
 		#in item['data']['tags'] which is a list of dicts of form {tag: tagname, type: value}
