54661fdc6088de3fb9f6f83a72b44422a20adb0d
persa188 <dev.persaud@mail.utoronto.ca>
2015-11-10 12:15:45 -0500

    d4 stuff

--- backup_tool/tester.py
+++ backup_tool/tester.py
@@ -3,7 +3,7 @@ from btool import BTool
 zot = zotero.Zotero('2704725', 'user', '4lXXdzNY1Io2632823WC0lcF')
 
 b = BTool('2704725', 'user', 'o2Zml5NwMI5JwZD6QHFR7O8S')
-#b.backup()
-#b.restore("27047252")
+b.backup()
+b.restore("27047252")
 
 print b.savelog()
