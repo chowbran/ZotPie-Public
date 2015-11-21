3d3dc9bf627a97324a5f1c9115fe223251d57279
persa188 <dev.persaud@mail.utoronto.ca>
2015-11-21 10:33:17 -0500

    some revisions made

--- testing grounds/cfapi.js
+++ testing grounds/cfapi.js
@@ -2,7 +2,7 @@
   use in verbose mode for debugging purposes
 */
 var sqlite3 = require('sqlite3').verbose();
-
+data=[];
 /*makes a new database at the specified path, note the path should include
   the database name and extension. (ex. ~/user/sanic/db.sqlite)
  */
@@ -13,46 +13,39 @@ function makedb(path){
 	var check;
     db.serialize(function() {
 	  //create DB with default fields itemID and info to store default info
-	  db.run("CREATE TABLE if not exists item_data (itemID TEXT, info TEXT)");
+	  db.run("CREATE TABLE if not exists item_data (itemID TEXT, field TEXT, value TEXT)");
 	});
 
 	db.close();
 }
 
-/*this function adds a column with name column_name to a table with name table_name 
-  at a db at path db_path.
-*/
-function add_column(db_path, table_name, column_name){
-	/*connect to db and add column to table*/
+
+function insert_item(db_path, itemID, field, value){
 	var db = new sqlite3.Database(db_path);
 	db.serialize(function(){
-		db.run('ALTER TABLE ' +table_name+' ADD '+column_name);
+		db.run('INSERT INTO item_data(itemID, field, value)' +" VALUES('"+itemID+"','"
+															 +field+"','"+ value + "');");
 	});
 	db.close();
 }
 
-function remove_column(db_path, table_name, column_name){
+function get_item(db_path, itemID){
 	var db = new sqlite3.Database(db_path);
-	//TODO, kind of lengthy and need to figure out a way to do this
-	//programattically or switch to SQL instead of SQLite
-}
-
-/* @psuedo docstring
-	insert item in table table_name at sqlite database at path db_path with column values 
-	being the columns in which you want to insert item data for and item_values are the
-	corresponding item valuses.
-	col_values is formatted as follows: a tuple casted to string with data e.x. (a,b,c)
-	item_values is formatted the same way.
-	the itemID field is important and is the only required field
-*/
-function insert_item(db_path, table_name, col_values, item_values){
-	var db = new sqlite3.Database(db_path);
-	db.serialize(function(){
-		db.run('INSERT INTO ' +table_name+ col_values +' VALUES'+item_values);
+	db.serialize(function() {
+		db.all("SELECT * FROM item_data WHERE itemID="+itemID, function(err, row){
+			//console.log(row);
+			//data.push(row);
+	}, function (err, rows){
+			//console.log(rows + "ROWS");
+			save(rows);
+		});
 	});
-	db.close();
 }
 
+function save(rows){
+	console.log(rows);
+	data.push(rows);
+}
 /*
 deletes item with itemID from table, itemID is a string surrounded by single qoutes
 such as itemID="'itemid'" -in this case the double qoutes suppress the single qoutes,
@@ -60,12 +53,14 @@ you can also use \'itemid\'
 */
 function remove_item(db_path, table_name, itemID){
 	var db = new sqlite3.Database(db_path);
-	db.serialize(function(){
-		db.run('DELETE FROM ' + table_name +' WHERE itemID = '+itemID);
-	});
-	db.close();
 }
+
+function retrieve_data(){return data;}
 /*commands for testing*/
+//makedb("./my22.sqlite");
 //add_column('./mydb4.sqlite', 'item_data', 'testcol1 TEXT');
-//insert_item('./mydb4.sqlite', 'item_data', "('itemID', 'info')", "('testid1','test2')");
+//insert_item('./my22.sqlite', '654', "custom2", "vsef2");
+get_item("./my22.sqlite", "654");
+console.log(data);
+retrieve_data();
 //remove_item('./mydb4.sqlite', 'item_data', "'testid1'");
