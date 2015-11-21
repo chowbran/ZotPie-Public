1bcdd8df0745723a1d32c0219ecb3bc9b035c5ec
Kenny Lam <kenny.lam@mail.utoronto.ca>
2015-11-21 02:11:30 -0500

    Added custom fields tab

--- ZotPieJS/ZotPie/chrome/content/ZotPie/customFields.js
+++ ZotPieJS/ZotPie/chrome/content/ZotPie/customFields.js
@@ -34,7 +34,166 @@ var ZotPieCustomFields = new function () {
 
             tabpanels.appendChild(cdTabPanel);
         }
+
+        // Subscribe function to event when item in tree is clicked
+        var itemsTree = document.getElementById('zotero-items-tree');
+        itemsTree.addEventListener("click", updateCustomData, true);
       
+    },
+
+    updateCustomData = function (event) {
+        var items = ZoteroPane.getSelectedItems();
+
+        if (items.length < 2) {
+            
+            // Remove old child nodes
+            var cdTabPanel = document.getElementById("zotpie-customdatapanel");
+
+            while (cdTabPanel.firstChild) {
+                cdTabPanel.removeChild(cdTabPanel.firstChild);
+            }
+
+            var vbox = document.createElement("vbox");
+            vbox.setAttribute("flex", "1");
+            vbox.setAttribute("style", "overflow:auto");
+            cdTabPanel.appendChild(vbox);
+
+            var addButton = document.createElement("button");
+            addButton.setAttribute("label", "Add");
+            addButton.setAttribute("oncommand", "addRow(this)");
+            vbox.appendChild(addButton);
+
+            var item = items[0];
+            console.log(item.id);
+
+            var grid = document.createElement("grid");
+            grid.setAttribute("style", "margin:10px");
+            var cols = document.createElement("columns");
+            var col1 = document.createElement("column");
+            col1.setAttribute("flex", "1");
+            var col2 = document.createElement("column");
+            col2.setAttribute("flex", "1");
+            var col3 = document.createElement("column");
+            col3.setAttribute("flex", "1");
+
+            cols.appendChild(col1);
+            cols.appendChild(col2);
+            cols.appendChild(col3);
+
+            var rows = document.createElement("rows");
+            var row1 = document.createElement("row");
+
+            var fieldName = document.createElement("label");
+            fieldName.setAttribute("value", "Field Name");
+            fieldName.setAttribute("style", "font-weight:bold;margin-right:100px");
+            fieldName.setAttribute("flex", "1");
+            var fieldData = document.createElement("label");
+            fieldData.setAttribute("value", "Data");
+            fieldData.setAttribute("style", "font-weight:bold");
+            fieldData.setAttribute("flex", "1");
+
+            row1.appendChild(fieldName);
+            row1.appendChild(fieldData);
+            rows.appendChild(row1);
+
+            vbox.appendChild(grid);
+            grid.appendChild(rows);
+
+
+            //var data = db.getData();
+            var data = [["cool", "aaazxczxlckzxclkzlca"], ["bees", "knees"]];
+            for (i = 0; i < data.length; i++) {
+                var d = data[i];
+                var row = document.createElement("row");
+
+                var t = document.createElement("label");
+                t.setAttribute("flex", "1");
+                //t.setAttribute("width", "150");
+                t.addEventListener("click", function (event) {
+                    if (event.button) {
+                        return;
+                    }
+                    showTextbox(this);
+                }, false);
+
+                t.className = "zotero-clicky";
+                t.setAttribute("value", data[i][0]);
+
+                var t2 = document.createElement("label");
+                t2.setAttribute("flex", "1");
+                t2.addEventListener("click", function (event) {
+                    if (event.button) {
+                        return;
+                    }
+                    showTextbox(this);
+                }, false);
+
+                t2.className = "zotero-clicky";
+                t2.setAttribute("value", data[i][1]);
+
+                var removeButton = document.createElement('label');
+                //removeButton.setAttribute("flex", "1");
+                removeButton.setAttribute("value", "-");
+                removeButton.setAttribute("class", "zotero-clicky zotero-clicky-minus");
+                removeButton.setAttribute("onclick", "removeRow(this)");
+
+                row.appendChild(t);
+                row.appendChild(t2);
+                row.appendChild(removeButton);
+
+                rows.appendChild(row);
+            }
+        }
+
+        
+
+    },
+
+    addRow = function (elem) {
+
+    },
+
+    removeRow = function (elem) {
+
+    },
+
+    showTextbox = function (elem) {
+        var t = document.createElement("textbox");
+        t.setAttribute('value', elem.getAttribute("value"));
+        //t.setAttribute('fieldname', fieldName);
+        //t.setAttribute('ztabindex', tabindex);
+        t.setAttribute('flex', '1');
+        //t.setAttribute("width", "120");
+        t.setAttribute('onblur', 'hideTextbox(this, this.value)');
+
+        var box = elem.parentNode;
+        box.replaceChild(t, elem);
+
+        t.focus();
+
+        var val = t.value;
+        t.value = '';
+        t.value = val;
+        //t.setAttribute('onkeypress', "return document.getBindingParent(this).handleKeyPress(event)");
+        //t.setAttribute('onpaste', "return document.getBindingParent(this).handlePaste(event)");
+    },
+
+    hideTextbox = function (elem, value) {
+        var t = document.createElement("label");
+        t.setAttribute("flex", "1");
+        //t.setAttribute("width", "150");
+        t.addEventListener("click", function (event) {
+            if (event.button) {
+                return;
+            }
+            showTextbox(this);
+        }, false);
+
+        t.className = "zotero-clicky";
+        t.setAttribute("value", value);
+
+        var box = elem.parentNode;
+        box.replaceChild(t, elem);
     },
 
     addToBatchEditQueue = function () {
