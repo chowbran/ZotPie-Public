e7990f6df218f71b811a3756bde13fbbefb1f608
Kenny Lam <kenny.lam@mail.utoronto.ca>
2015-11-20 21:34:36 -0500

    Created startup window

--- ZotPieJS/ZotPie/chrome/content/ZotPie/customFields.js
+++ ZotPieJS/ZotPie/chrome/content/ZotPie/customFields.js
@@ -1,103 +1,86 @@
-var ZoteroPane = Components.classes["@mozilla.org/appshell/window-mediator;1"] .getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser").ZoteroPane;
-Zotero.CustomFields = {
-	DB: null,
-	
-	init: function () {
-		// Connect to (and create, if necessary) helloworld.sqlite in the Zotero directory
-		this.DB = new Zotero.DBConnection('zotero');
-		
-		if (!this.DB.tableExists('changes')) {
-			this.DB.query("CREATE TABLE changes (num INT)");
-			this.DB.query("INSERT INTO changes VALUES (0)");
-		}
+var ZotPieCustomFields = new function () {
+    var tab, tabpanels;
 
-		// Register the callback in Zotero as an item observer
-		var notifierID = Zotero.Notifier.registerObserver(this.notifierCallback, ['item']);
-		
-		// Unregister callback when the window closes (important to avoid a memory leak)
-		window.addEventListener('unload', function(e) {
-				Zotero.Notifier.unregisterObserver(notifierID);
-		}, false);
-	},
+    var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                              .getService(Components.interfaces.nsIPromptService);
 
-	batchTagEditTest: function () {
-	},
-	
-	insertHello: function() {
-		var data = {
-			title: "Zotero",
-			company: "Center for History and New Media",
-			creators: [
-				['Dan', 'Stillman', 'programmer'],
-				['Simon', 'Kornblith', 'programmer']
-			],
-			version: '1.0.1',
-			company: 'Center for History and New Media',
-			place: 'Fairfax, VA',
-			url: 'http://www.zotero.org'
-		};
-		Zotero.Items.add('computerProgram', data); // returns a Zotero.Item instance
-	},
-	
-	// Callback implementing the notify() method to pass to the Notifier
-	notifierCallback: {
-		notify: function(event, type, ids, extraData) {
-			if (event == 'add' || event == 'modify' || event == 'delete') {
-				// Increment a counter every time an item is changed
-				Zotero.BatchEditor.DB.query("UPDATE changes SET num = num + 1");
-				
-				if (event != 'delete') {
-					// Retrieve the added/modified items as Item objects
-					var items = Zotero.Items.get(ids);
-				}
-				else {
-					var items = extraData;
-				}
-				
-				// Loop through array of items and grab titles
-				var titles = [];
-				for each(var item in items) {
-					// For deleted items, get title from passed data
-					if (event == 'delete') {
-						titles.push(item.old.title ? item.old.title : '[No title]');
-					}
-					else {
-						titles.push(item.getField('title'));
-					}
-				}
-				
-				if (!titles.length) {
-					return;
-				}
-				
-				// Get the localized string for the notification message and
-				// append the titles of the changed items
-				var stringName = 'notification.item' + (titles.length==1 ? '' : 's');
-				switch (event) {
-					case 'add':
-						stringName += "Added";
-						break;
-						
-					case 'modify':
-						stringName += "Modified";
-						break;
-						
-					case 'delete':
-						stringName += "Deleted";
-						break;
-				}
-				
-				var str = document.getElementById('zotpie-zotero-strings').
-					getFormattedString(stringName, [titles.length]) + ":\n\n" +
-					titles.join("\n");
-			}
-			
-			var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-				.getService(Components.interfaces.nsIPromptService);
-			ps.alert(null, "", str);
-		}
-	}
-};
+    var ZoteroPane = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+    .getService(Components.interfaces.nsIWindowMediator)
+    .getMostRecentWindow("navigator:browser").ZoteroPane;
 
-// Initialize the utility
-window.addEventListener('load', function(e) { Zotero.BatchEditor.init(); }, false);
+    this.onLoad = function () {
+
+        tab = document.getElementById("zotero-view-tabbox");
+
+        if (tab) {
+            console.log(tab.getAttribute("id"));
+
+            var cdTab = document.createElement("tab");
+            cdTab.setAttribute("id", "zotpie-customdatatab");
+            cdTab.setAttribute("label", "Custom Data");
+            cdTab.setAttribute("value", "customdata");
+            tab.firstChild.appendChild(cdTab);
+            //console.log(zoteroTab.firstChild.);
+            //zoteroTab.firstChild.setAttribute("id", "zotpie-tabs");
+        }
+
+        tabpanels = document.getElementById("zotero-view-item");
+
+        if (tabpanels) {
+            var cdTabPanel = document.createElement("tabpanel");
+            cdTabPanel.setAttribute("id", "zotpie-customdatapanel");
+            //var zoterobox = document.createElement("zoteroitembox");
+            //cdTabPanel.appendChild(zoterobox);
+
+            tabpanels.appendChild(cdTabPanel);
+        }
+      
+    },
+
+    addToBatchEditQueue = function () {
+        var selectedItems = ZoteroPane.getSelectedItems();
+        var item = selectedItems[0];
+        var counter = 0;
+
+        for (i = 0; i < selectedItems.length; i++) {
+            if (batchedItems.indexOf(selectedItems[i].id) == -1) {
+                batchedItems.push(selectedItems[i].id);
+                counter++;
+            }
+        }
+
+        var flags = ps.BUTTON_POS_0 * ps.BUTTON_TITLE_IS_STRING +
+            ps.BUTTON_POS_1 * ps.BUTTON_TITLE_IS_STRING +
+            ps.BUTTON_POS_2 * ps.BUTTON_TITLE_IS_STRING;
+
+        var button;
+
+        if (counter == 0) {
+            button = ps.confirmEx(null, "Warning", "Items are already in the batch edit queue.",
+                   flags, "Open Batch Editor", "Continue Adding", null, null, {});
+        } else {
+            button = ps.confirmEx(null, "Success!", "Added " + counter + " items to the tag batch edit queue.\nThere are currently "
+                                + batchedItems.length.toString() + " items in the queue.",
+                               flags, "Open Batch Editor", "Continue Adding", null, null, {});
+        }
+
+        if (button == 0) {
+            Zotero.ZotPie.startBatchEditor();
+        }
+
+        //toastr.success("test");
+
+    },
+
+    this.onUnload = function () {
+
+    }
+
+    this.onBeforeUnload = function () {
+
+    }
+}
+
+window.addEventListener("load", function (e) { ZotPieCustomFields.onLoad(e); }, false);
+window.addEventListener("unload", function (e) { ZotPieCustomFields.onUnload(e); }, false);
+window.addEventListener("beforeunload", function (e) { ZotPieCustomFields.onBeforeUnload(e); }, false);
